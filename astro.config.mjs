0859de5e41568b5452f3302f4a840d127066535e
AlanSwenson <alan@alanswenson.dev>
2025-02-11 10:00:36 -0600

    change over domain on sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://ashleybegin.com',
+    site: 'https://spacekraftrecords.com',
     integrations: [
         tailwind(),
         sitemap({
